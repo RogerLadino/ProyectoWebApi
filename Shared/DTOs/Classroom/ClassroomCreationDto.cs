e16939995da742f69352e1e7f94cf300009658f3
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-23 15:32:16 -0500

    feat: Añade creation, update y el Dto normal para classroom

---
+++ Shared/DTOs/Classroom/ClassroomCreationDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Shared.DTOs.Classroom;
+public partial class ClassroomCreationDto
+{
+
+    [Required(ErrorMessage = "El nombre del aula es requerido")]
+    [StringLength(45, ErrorMessage = "El nombre no puede exceder los 45 caracteres")]
+    [Display(Name = "Nombre del Aula")]
+    public string Name { get; set; } = string.Empty;
+
+}
+
---
+++ Shared/DTOs/Classroom/ClassroomDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.DTOs.Classroom;
+public partial class ClassroomDto
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+
+}
