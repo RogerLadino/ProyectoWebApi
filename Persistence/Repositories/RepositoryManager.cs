d6865c3fc80fa49cb954f75638bac775b53f20f7
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-23 15:14:05 -0500

    feat: Añade IClassroomRepository

--- Persistence/Repositories/RepositoryManager.cs
+++ Persistence/Repositories/RepositoryManager.cs
@@ -7,13 +7,17 @@ namespace Persistence.Repositories
         private readonly RepositoryDbContext _dbContext;
 
         private readonly Lazy<IExerciseRepository> _lazyExerciseRepository;
+        private readonly Lazy<IClassroomRepository> _lazyClassroomRepository;
 
         public RepositoryManager(RepositoryDbContext dbContext)
         {
             _dbContext = dbContext;
 
             _lazyExerciseRepository = new Lazy<IExerciseRepository>(() => new ExerciseRepository(_dbContext));
+            
+            _lazyClassroomRepository = new Lazy<IClassroomRepository>(() => new ClassroomRepository(_dbContext));
         }
+        public IClassroomRepository ClassroomRepository => _lazyClassroomRepository.Value;   
 
         public IExerciseRepository ExerciseRepository =>  _lazyExerciseRepository.Value;
         public async Task SaveChangesAsync()
