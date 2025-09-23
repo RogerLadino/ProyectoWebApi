a8ae120e27498cc520754167a7c9976d2a99b099
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-23 15:27:50 -0500

    feat: Llamado de servicio base y de servicio de classroom

---
+++ Persistence/Repositories/ClassroomRepository.cs
@@ -0,0 +1,12 @@
+﻿using Domain.Entities;
+using Domain.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Repositories;
+
+public class ClassroomRepository : RepositoryBase<Classroom>, IClassroomRepository
+{
+    public ClassroomRepository(RepositoryDbContext repositorycontext) : base(repositorycontext)
+    {
+    }
+}
