bb0899b43645e2c0792653bd374cc3d446ed6914
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-23 15:22:51 -0500

    feat: Añade IclassroomService

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -5,11 +5,13 @@ using Services;
 public sealed class ServiceManager : IServiceManager
 {
     private readonly Lazy<IExerciseService> _lazyExerciseService;
-
+    private readonly Lazy<IClassroomService> _lazyClassroomService;
     public ServiceManager(IRepositoryManager repositoryManager)
     {
         _lazyExerciseService = new Lazy<IExerciseService>(() => new ExerciseService(repositoryManager));
+        _lazyClassroomService = new Lazy<IClassroomService>(() => new ClassroomService(repositoryManager));
     }
 
     public IExerciseService ExerciseService => _lazyExerciseService.Value;
+    public IClassroomService ClassroomService => _lazyClassroomService.Value;
 }
