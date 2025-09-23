7fbb2d9b3e6845b814e8e7456c6d38e36f3aafd9
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-23 15:30:54 -0500

    feat: Añade IClassroomService

---
+++ Service.Abstractions/IClassroomService.cs
@@ -0,0 +1,9 @@
+﻿using Shared.DTOs.Classroom;
+
+namespace Service.Abstractions;
+
+public interface IClassroomService : IServiceBase<ClassroomDto>
+{
+    Task<ClassroomDto> GetByCodeAsync(string code);
+    Task<ClassroomDto> CreateAsync(ClassroomCreationDto classroomForCreationDto);
+}
