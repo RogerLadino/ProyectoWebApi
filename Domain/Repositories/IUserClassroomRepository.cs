a013892d05b8ce80d67e66674a131327e0661cee
NICOLAS2006-APRENDIZ <nicolasvanegas171@gmail.com>
2025-09-28 15:53:19 -0500

    implementacion de iuserreporitory

--- Domain/Repositories/IUserClassroomRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿using Domain.Entities;
-using System.Linq.Expressions;
-using System.Threading.Tasks;
-
-namespace Domain.Repositories;
-public interface IUserClassroomRepository : IRepositoryBase<Classroom>
-{
-}
