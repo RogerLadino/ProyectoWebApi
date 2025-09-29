650489c724bef770a14021dfbadd9b48e1e8e94b
Roger Ladino <ztroger15@gmail.com>
2025-09-29 03:47:54 -0500

    feat (submissions): Agrega el uso de autorización

--- Presentation/Controllers/SubmissionController.cs
+++ Presentation/Controllers/SubmissionController.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Service.Abstractions;
 using Shared.DTOs.Submission;
+using System.Security.Claims;
 
+[Authorize]
 [ApiController]
-[Route("api/classroom/{classroomId:int}/exercise/{exerciseId:int}/submission")]
+[Route("api/exercise/{exerciseId:int}/submission")]
 public class SubmissionController : ControllerBase
 {
     private readonly IServiceManager _serviceManager;
@@ -11,6 +14,7 @@ public class SubmissionController : ControllerBase
     public SubmissionController(IServiceManager serviceManager)
         => _serviceManager = serviceManager;
 
+    [Authorize(Roles = "Profesor")]
     [HttpGet]
     public async Task<IActionResult> GetAllSubmissions([FromRoute] int exerciseId)
     {
@@ -18,13 +22,19 @@ public class SubmissionController : ControllerBase
         return Ok(submissionDto);
     }
 
-    [HttpGet("{userId:int}")]
-    public async Task<IActionResult> GetSubmissionById([FromRoute] int userId, [FromRoute] int exerciseId)
+    [HttpGet("user")]
+    public async Task<IActionResult> GetSubmissionById([FromRoute] int exerciseId)
     {
+        var userIdStr = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        if (string.IsNullOrEmpty(userIdStr) || !int.TryParse(userIdStr, out var userId))
+            return Unauthorized("UserId not found or invalid");
+
         var submissionDto = await _serviceManager.SubmissionService.GetByIdAsync(userId, exerciseId);
         return Ok(submissionDto);
     }
 
+    [Authorize(Roles = "Profesor")]
     [HttpPut]
     public async Task<IActionResult> AssignNote([FromRoute] int exerciseId, [FromBody] AssignGradeDto assignGradeDto)
     {
