ebdcb6c7a5ff144c647bb803cae353a513e525cc
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-12-08 12:21:31 -0500

    feat: Añade nueva carpeta Filters y incorpora filtro para logout

---
+++ Presentation/Filters/TokenRevocationFilter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Service.Abstractions;
+
+public class TokenRevocationFilter : IAuthorizationFilter
+{
+    private readonly ITokenBlacklistService _tokenBlacklistService;
+
+    public TokenRevocationFilter(ITokenBlacklistService tokenBlacklistService)
+    {
+        _tokenBlacklistService = tokenBlacklistService;
+    }
+
+    public void OnAuthorization(AuthorizationFilterContext context)
+    {
+        var token = context.HttpContext.Request.Headers["Authorization"].ToString().Replace("Bearer ", "");
+
+        if (!string.IsNullOrEmpty(token) && _tokenBlacklistService.IsTokenRevoked(token))
+        {
+            context.Result = new UnauthorizedObjectResult("Token revocado.");
+        }
+    }
+}
