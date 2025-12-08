3f5b91fb2944f0faa183e7e9ea26a364161147dc
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-12-08 12:22:43 -0500

    feat: Agrega nuevo servicio de logout y su abstracción

---
+++ Service.Abstractions/ITokenBlacklistService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks;
+
+public interface ITokenBlacklistService
+{
+    void RevokeToken(string token);
+    bool IsTokenRevoked(string token);
+}
---
+++ Services/TokenBlacklistService.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Concurrent;
+using Service.Abstractions;
+using Core.Services;
+using Core.Services.Abstractions;
+using Domain.Realtime;
+using Domain.Repositories;
+using Microsoft.Extensions.Configuration;
+using Services;
+
+public class TokenBlacklistService : ITokenBlacklistService
+{
+    private readonly ConcurrentDictionary<string, DateTime> _revokedTokens = new();
+
+    public void RevokeToken(string token)
+    {
+        _revokedTokens.TryAdd(token, DateTime.UtcNow);
+    }
+
+    public bool IsTokenRevoked(string token)
+    {
+        return _revokedTokens.ContainsKey(token);
+    }
+}
