e80f2b6e8693c18ce43540500fc8d4e5d6b28c80
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-12-08 12:19:47 -0500

    fix: Remueve servicio obsoleto de revocacion de tokens

--- Service.Abstractions/ITokenRevocationService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Core.Services.Abstractions;
-
-public interface ITokenRevocationService
-{
-    void RevokeToken(string jti, DateTime expiresUtc);
-    bool IsTokenRevoked(string jti);
-}
