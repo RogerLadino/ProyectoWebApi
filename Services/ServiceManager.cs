a9221b1709b5fd480982d7448e28042e9da71bdf
TomasSanchez119 <tsanchezavellaneda@gmail.com>
2025-12-09 17:38:11 -0500

    fix: Declarar clase dentro de un namespace

--- Services/ServiceManager.cs
+++ Services/ServiceManager.cs
@@ -6,34 +6,37 @@ using Microsoft.Extensions.Configuration;
 using Service.Abstractions;
 using Services;
 
-public sealed class ServiceManager : IServiceManager
+namespace Infrastructure.Services
 {
-    private readonly Lazy<IExerciseService> _lazyExerciseService;
-    private readonly Lazy<IClassroomService> _lazyClassroomService;
-    private readonly Lazy<ISubmissionService> _lazySubmissionService;
-    private readonly Lazy<ICodeService> _lazyCodeService;
-    private readonly Lazy<IEmailService> _lazyEmailService;
-    private readonly Lazy<IAuthService> _lazyAuthService;
-
-    public ServiceManager(IRepositoryManager repositoryManager, IRealtimeManager realtimeManager, IConfiguration configuration)
+    public sealed class ServiceManager : IServiceManager
     {
-        _lazyClassroomService = new Lazy<IClassroomService>(() => new ClassroomService(repositoryManager));
-        _lazyExerciseService = new Lazy<IExerciseService>(() => new ExerciseService(repositoryManager));
-        _lazyEmailService = new Lazy<IEmailService>(() => new EmailService(configuration));
-        _lazyAuthService = new Lazy<IAuthService>(() => new AuthService(repositoryManager, _lazyEmailService.Value, configuration));
-        _lazySubmissionService = new Lazy<ISubmissionService>(() => new SubmissionService(repositoryManager));
-        _lazyCodeService = new Lazy<ICodeService>(() => new CodeService(repositoryManager, realtimeManager));
+        private readonly Lazy<IExerciseService> _lazyExerciseService;
+        private readonly Lazy<IClassroomService> _lazyClassroomService;
+        private readonly Lazy<ISubmissionService> _lazySubmissionService;
+        private readonly Lazy<ICodeService> _lazyCodeService;
+        private readonly Lazy<IEmailService> _lazyEmailService;
+        private readonly Lazy<IAuthService> _lazyAuthService;
+
+        public ServiceManager(IRepositoryManager repositoryManager, IRealtimeManager realtimeManager, IConfiguration configuration)
+        {
+            _lazyClassroomService = new Lazy<IClassroomService>(() => new ClassroomService(repositoryManager));
+            _lazyExerciseService = new Lazy<IExerciseService>(() => new ExerciseService(repositoryManager));
+            _lazyEmailService = new Lazy<IEmailService>(() => new EmailService(configuration));
+            _lazyAuthService = new Lazy<IAuthService>(() => new AuthService(repositoryManager, _lazyEmailService.Value, configuration));
+            _lazySubmissionService = new Lazy<ISubmissionService>(() => new SubmissionService(repositoryManager));
+            _lazyCodeService = new Lazy<ICodeService>(() => new CodeService(repositoryManager, realtimeManager));
+        }
+
+        public IEmailService EmailService => _lazyEmailService.Value;
+
+        public IAuthService AuthService => _lazyAuthService.Value;
+
+        public IExerciseService ExerciseService => _lazyExerciseService.Value;
+
+        public ISubmissionService SubmissionService => _lazySubmissionService.Value;
+
+        public ICodeService CodeService => _lazyCodeService.Value;
+
+        public IClassroomService ClassroomService => _lazyClassroomService.Value;
     }
-
-    public IEmailService EmailService => _lazyEmailService.Value;
-
-    public IAuthService AuthService => _lazyAuthService.Value;
-
-    public IExerciseService ExerciseService => _lazyExerciseService.Value;
-
-    public ISubmissionService SubmissionService => _lazySubmissionService.Value;
-
-    public ICodeService CodeService => _lazyCodeService.Value;
-
-    public IClassroomService ClassroomService => _lazyClassroomService.Value;
 }
