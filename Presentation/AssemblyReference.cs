d6323aefc8be85fe933c81035b599e956d8936ab
RogerLadino <ztroger15@gmail.com>
2025-12-10 02:06:56 -0500

    fix: Cambia de clase a interfaz

--- Presentation/AssemblyReference.cs
+++ Presentation/AssemblyReference.cs
@@ -4,7 +4,7 @@
     /// Clase utilizada como marcador para obtener una referencia al ensamblado Presentation.
     /// Permite registrar dependencias o cargar tipos mediante reflexión sin necesidad de instanciar objetos.
     /// </summary>
-    public class AssemblyReference
+    public interface AssemblyReference
     {
         // Intencionalmente vacía: actúa solo como referencia al ensamblado Presentation.
     }
